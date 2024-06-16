5e6ae0d396ef88d8733e26c87595e92c25c3bcd0
Akash Khan <akashcc48@gmail.com>
2024-06-16 12:47:54 +0600

    contentHtml show on the ui

--- components/ContentDisplay.jsx
+++ components/ContentDisplay.jsx
@@ -4,7 +4,7 @@ import Tag from "./Tag";
 
 const ContentDisplay = async ({ id }) => {
   const documentContent = await getDocumentContent(id);
-
+  console.log(documentContent);
   return (
     <article className="prose dark:prose-invert">
       <h1>{documentContent.title}</h1>
@@ -23,6 +23,10 @@ const ContentDisplay = async ({ id }) => {
         {documentContent.tags &&
           documentContent.tags.map((tag) => <Tag key={tag} tag={tag} />)}
       </div>
+      <div
+        className="lead "
+        dangerouslySetInnerHTML={{ __html: documentContent.contentHtml }}
+      />
     </article>
   );
 };
